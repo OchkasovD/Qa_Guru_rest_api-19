24aa0c211a706d0b789b4ee9d8a447a4181883ee
d.ochkasov <d.ochkasov@qiwi.com>
2023-06-10 20:33:53 +0300

    init

--- src/test/java/TestsAPI/ApiTests.java
+++ src/test/java/TestsAPI/ApiTests.java
@@ -5,24 +5,26 @@ import static io.restassured.RestAssured.*;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.*;
 public class ApiTests extends TestBase {
+
+
     @Test
     @DisplayName("REGISTER - SUCCESSFUL")
     void successLoginTest() {
         String requestBody = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"pistol\" }"; // BAD PRACTICE
 
-        given()
-                .log().uri()
-                .log().body()
-                .contentType(JSON)
-                .body(requestBody)
-                .when()
-                .post("register")
-                .then()
-                .log().status()
-                .log().body()
-                .statusCode(200)
-                .body("id", is(4))
-                .body("token", is("QpwL5tke4Pnpja7X4"));
+       given()
+               .log().uri()
+               .log().body()
+               .contentType(JSON)
+               .body(requestBody)
+               .when()
+               .post("register")
+               .then()
+               .log().status()
+               .log().body()
+               .statusCode(200)
+               .body("id", is(4))
+               .body("token", is("QpwL5tke4Pnpja7X4"));
     }
     }
 
