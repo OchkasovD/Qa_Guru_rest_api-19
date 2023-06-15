ee2d0da797de6beb945bf6c5c90985ae02f2c105
d.ochkasov <d.ochkasov@qiwi.com>
2023-06-15 21:00:17 +0300

    init fix name Add Tests fix build.gradle add steps

--- src/test/java/tests/ReqresInExtendedTests.java
+++ src/test/java/tests/ReqresInExtendedTests.java
@@ -1,36 +1,41 @@
 package tests;
-import models.LoginBodyModel;
-import models.LoginResponseModel;
+import static helpers.CustomAllureListener.withCustomTemplates;
+import models.lombok.LoginBodyLombokModel;
+import models.lombok.LoginResponseLombokModel;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 
+import static io.qameta.allure.Allure.step;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReqresInExtendedTests extends TestBase {
 
     @Test
     @DisplayName("LOGIN - SUCCESSFUL")
-    void successLoginWithPojoModelsTest() {
-
-        LoginBodyModel requestBody = new LoginBodyModel();
-        requestBody.setEmail("eve.holt@reqres.in");
-        requestBody. setPassword("cityslicka");
-
-        LoginResponseModel loginResponse = given()
-                .log().uri()
-                .log().body()
-                .contentType(JSON)
-                .body(requestBody)
-                .when()
-                .post("login")
-                .then()
-                .log().status()
-                .log().body()
-                .statusCode(200)
-                .extract().as(LoginResponseModel.class);
-assertEquals("QpwL5tke4Pnpja7X4", loginResponse.getToken());
+    void successLoginWithLombokModelsTest() {
+        LoginBodyLombokModel requestBody = new LoginBodyLombokModel();
+        step("Prepare test data" , () -> {
+                requestBody.setEmail("eve.holt@reqres.in");
+        requestBody.setPassword("cityslicka");
+});
+        LoginResponseLombokModel loginResponse = step("Make request", () ->
+                 given()
+                        .log().uri()
+                        .log().body()
+                        .filter(withCustomTemplates())
+                        .contentType(JSON)
+                        .body(requestBody)
+                        .when()
+                        .post("login")
+                        .then()
+                        .log().status()
+                        .log().body()
+                        .statusCode(200)
+                        .extract().as(LoginResponseLombokModel.class));
+       step("Check response", () ->
+        assertEquals("QpwL5tke4Pnpja7X4", loginResponse.getToken()));
 
     }
 
